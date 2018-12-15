0388cb72e279ac35cd453d0b34abc38d894e412b
Dakota Lorance <admin@slyb--ts.com>
2018-12-15 11:39:26 -0600

    updated utils module
    added a container component and jsdoc comments to everything

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+/** __A generic header component__ */
 export const Header = props => {
   const content = props.linkto ? (
     <a href="{props.linkto}" target="_blank">
@@ -16,6 +17,16 @@ export const Header = props => {
   );
 };
 
+/** __Generic container component__ */
+export const Container = props => {
+  return (
+    <div className={"container " + (props.className || "")}>
+      {props.children}
+    </div>
+  );
+};
+
+/** __A separator that can be placed between sections of text__ */
 export const Separator = props => {
   return <>&nbsp;{props.use || "|"}&nbsp;</>;
 };
