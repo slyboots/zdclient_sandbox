764a73fb90c4b66bbea9798e204e1fc73fdc2665
Dakota Lorance <admin@slyb--ts.com>
2018-12-15 21:31:50 -0600

    Got rid of the shortcuts module and the board data section is looking good

--- src/sitedetails.js
+++ src/sitedetails.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Header, Separator } from "./utils";
 import { Detail, DetailGroup, Flag } from "./details";
 
-import { BoardShortcutContainer } from "./shortcuts";
-
 const SupportLinkGroup = props => {
   const site = props.site;
   const links = [
@@ -25,6 +23,61 @@ const SupportLinkGroup = props => {
     </DetailGroup>
   );
 };
+export const ShortcutLink = props => {
+  return (
+    <a href={props.url || null} title={props.title} target="_blank">
+      {props.children}
+    </a>
+  );
+};
+
+const BoardShortcut = props => {
+  return (
+    <div>
+      <ShortcutLink url={props.ropeUrl} title="rope app">
+        Rope: #{props.boardId}
+      </ShortcutLink>
+      <Separator use="::" />
+      <ShortcutLink url={props.appUrl} title="rg boards app">
+        Boards App
+      </ShortcutLink>
+      <Separator use="::" />
+      <small>{props.updateTime}</small>
+    </div>
+  );
+};
+
+export const BoardShortcutContainer = props => {
+  const { boards } = props.site;
+  return (
+    <DetailGroup id={props.id}>
+      <Detail label="MCP" anchor={props.site.mcp_board_url} hint="RG MCP Board">
+        {props.site.board_name}
+      </Detail>
+      {boards.map((board, i) => {
+        return (
+          <DetailGroup inline>
+            <Detail
+              label="Rope"
+              id={board.board_id}
+              anchor={board.rope_url}
+              hint="Pulley MLS Data System"
+            >
+              #{board.board_id}
+            </Detail>
+            <Detail anchor={board.app_url} hint="Website Boards App">
+              Boards App
+            </Detail>
+            <Detail hint="MLS Data Last Updated Timestamp">
+              {board.update_time}
+            </Detail>
+          </DetailGroup>
+        );
+      })}
+    </DetailGroup>
+  );
+};
+
 const SiteStatusFlags = props => {
   const { site } = props;
   const mapper = {
