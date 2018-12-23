efd6f67860150270fa1a5b4114485003ef007b37
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-23 11:08:39 -0600

    removed backgroundcolor style since it isn;t needed anymore

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ import { Anchor } from "./details";
 export const Header = props => {
   const heading = React.createElement("h" + props.lvl, null, props.children); //<h3 className="heading">{props.children}</h3>;
   return (
-    <div style={{ position: "sticky", top: 0, backgroundColor: "lightgray" }}>
+    <div style={{ position: "sticky", top: 0}}>
       {props.linkto ? <Anchor to={props.linkto}>{heading}</Anchor> : heading}
       <hr />
     </div>
