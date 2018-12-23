01100cadb04d2b09254ebd390440d2df877cc049
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-23 11:09:02 -0600

    removed unused code and reformatted some otehr stuff

--- src/sitedetails.js
+++ src/sitedetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, Separator } from "./utils";
+import { Header } from "./utils";
 import { Detail, DetailGroup, Flag } from "./details";
 
 const SupportLinkGroup = props => {
@@ -23,29 +23,6 @@ const SupportLinkGroup = props => {
     </DetailGroup>
   );
 };
-export const ShortcutLink = props => {
-  return (
-    <a href={props.url || null} title={props.title} target="_blank">
-      {props.children}
-    </a>
-  );
-};
-
-const BoardShortcut = props => {
-  return (
-    <div>
-      <ShortcutLink url={props.ropeUrl} title="rope app">
-        Rope: #{props.boardId}
-      </ShortcutLink>
-      <Separator use="::" />
-      <ShortcutLink url={props.appUrl} title="rg boards app">
-        Boards App
-      </ShortcutLink>
-      <Separator use="::" />
-      <small>{props.updateTime}</small>
-    </div>
-  );
-};
 
 export const BoardShortcutContainer = props => {
   const { boards } = props.site;
@@ -55,21 +32,17 @@ export const BoardShortcutContainer = props => {
         {props.site.board_name}
       </Detail>
       {boards.map((board, i) => {
+        const {board_id,rope_url,app_url,update_time} = board
         return (
           <DetailGroup inline>
-            <Detail
-              label="Rope"
-              id={board.board_id}
-              anchor={board.rope_url}
-              hint="Pulley MLS Data System"
-            >
-              #{board.board_id}
+            <Detail label="Rope" anchor={rope_url} hint="Data mapping system">
+              #{board_id}
             </Detail>
-            <Detail anchor={board.app_url} hint="Website Boards App">
+            <Detail anchor={app_url} hint="Find MLS contact/billing info here">
               Boards App
             </Detail>
             <Detail hint="MLS Data Last Updated Timestamp">
-              {board.update_time}
+              {update_time}
             </Detail>
           </DetailGroup>
         );
